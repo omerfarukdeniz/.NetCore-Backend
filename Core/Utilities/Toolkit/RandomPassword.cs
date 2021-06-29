5e9840f137828e12fc599dfbb2fb3992c0610cc6
omerfarukdeniz <omerfarukdeniz75@gmail.com>
2021-06-29 16:10:40 +0300

    Added Random Password Generator

---
+++ Core/Utilities/Toolkit/RandomPassword.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Toolkit
+{
+    public static class RandomPassword
+    {
+        public static string CreateRandomPassword(int length = 14)
+        {
+            string validChars = "ABCDEFGHIJKLMNOPRSTUVWXYZabcdefghijklmnoprstuvwxyz0123456789!@#$%^&*?_-";
+            var random = new Random();
+
+            char[] chars = new char[length];
+            for (int i = 0; i < length; i++)
+            {
+                chars[1] = validChars[random.Next(0, validChars.Length)];
+            }
+            return new string(chars);
+        }
+
+        public static int RandomNumberGenerator(int min = 100000, int max = 999999)
+        {
+            var random = new Random();
+            return random.Next(min, max);
+        }
+    }
+}
