9f530c3886bc3c529ea625048773f936696084b5
omerfarukdeniz <omerfarukdeniz75@gmail.com>
2021-06-30 14:55:49 +0300

    Added RabbitMq

---
+++ Core/Utilities/MessageBrokers/RabbitMq/IMessageConsumer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.MessageBrokers.RabbitMq
+{
+    public interface IMessageConsumer
+    {
+        void GetQueue();
+    }
+}
