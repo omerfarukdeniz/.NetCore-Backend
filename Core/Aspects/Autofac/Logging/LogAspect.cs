443f6e7f12aa6b6dd13b64e13aa7cdf9fe9ac57a
omerfarukdeniz <omerfarukdeniz75@gmail.com>
2021-06-30 13:52:27 +0300

    Log Aspect added

---
+++ Core/Aspects/Autofac/Logging/LogAspect.cs
@@ -0,0 +1,58 @@
+﻿using Core.CrossCuttingConcerns.Logging.SeriLog;
+using Core.Utilities.Interceptors;
+using Core.Utilities.IoC;
+using Core.Utilities.Messages;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Extensions.DependencyInjection;
+using Castle.DynamicProxy;
+using Core.CrossCuttingConcerns.Logging;
+using Newtonsoft.Json;
+
+namespace Core.Aspects.Autofac.Logging
+{
+    public class LogAspect:MethodInterception
+    {
+        private readonly LoggerServiceBase _loggerServiceBase;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public LogAspect(Type loggerService)
+        {
+            if (loggerService.BaseType != typeof(LoggerServiceBase))
+            {
+                throw new ArgumentException(AspectMessages.WrongLoggerType);
+            }
+
+            _loggerServiceBase = (LoggerServiceBase)ServiceTool.ServiceProvider.GetService(loggerService);
+            _httpContextAccessor = ServiceTool.ServiceProvider.GetService<IHttpContextAccessor>();
+        }
+
+        private string GetLogDetail(IInvocation invocation)
+        {
+            var logParameters = new List<LogParameter>();
+            for (int i = 0; i < invocation.Arguments.Length; i++)
+            {
+                logParameters.Add(new LogParameter
+                {
+                    Name = invocation.GetConcreteMethod().GetParameters()[i].Name,
+                    Value = invocation.Arguments[i],
+                    Type = invocation.Arguments[i].GetType().Name
+                });
+            }
+            var logDetail = new LogDetail
+            {
+                MethodName = invocation.Method.Name,
+                Parameters = logParameters,
+                User = (_httpContextAccessor.HttpContext == null || _httpContextAccessor.HttpContext.User.Identity.Name == null) ? "?" : _httpContextAccessor.HttpContext.User.Identity.Name
+            };
+            return JsonConvert.SerializeObject(logDetail);
+        }
+
+        protected override void OnBefore(IInvocation invocation)
+        {
+            _loggerServiceBase?.Info(GetLogDetail(invocation));
+        }
+    }
+}
