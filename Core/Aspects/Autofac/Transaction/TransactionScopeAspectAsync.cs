e99cf8f5ea78c9cafe9cccc0d3185b4e7617ac48
omerfarukdeniz <omerfarukdeniz75@gmail.com>
2021-06-30 14:03:58 +0300

    Transaction aspect added

---
+++ Core/Aspects/Autofac/Transaction/TransactionScopeAspectAsync.cs
@@ -0,0 +1,45 @@
+﻿using Castle.DynamicProxy;
+using Core.Utilities.Interceptors;
+using Core.Utilities.IoC;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Transactions;
+
+namespace Core.Aspects.Autofac.Transaction
+{
+    public class TransactionScopeAspectAsync: MethodInterception
+    {
+        private readonly Type _dbContextType;
+        public TransactionScopeAspectAsync()
+        {
+
+        }
+        public void InterceptDbContext(IInvocation invocation)
+        {
+            var db = ServiceTool.ServiceProvider.GetService(_dbContextType) as DbContext;
+            using (var transactionScope = db.Database.BeginTransaction())
+            {
+                try
+                {
+                    invocation.Proceed();
+                    transactionScope.Commit();
+                }
+                finally
+                {
+                    transactionScope.Rollback();
+                }
+            }
+        }
+
+        public override void Intercept(IInvocation invocation)
+        {
+            using (var tx = new TransactionScope(TransactionScopeAsyncFlowOption.Enabled))
+            {
+                invocation.Proceed();
+                tx.Complete();
+            }
+        }
+    }
+}
