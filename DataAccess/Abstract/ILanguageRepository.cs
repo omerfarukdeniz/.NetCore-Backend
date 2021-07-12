bc6809a8f9d74372fd412b5db14f1c540702f5d0
omerfarukdeniz <omerfarukdeniz75@gmail.com>
2021-07-12 13:35:44 +0300

    Abstract Repositories Added

---
+++ DataAccess/Abstract/ILanguageRepository.cs
@@ -0,0 +1,16 @@
+﻿using Core.DataAccess;
+using Core.Entities.Concrete;
+using Core.Entities.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Abstract
+{
+    public interface ILanguageRepository:IEntityRepository<Language>
+    {
+        Task<List<SelectionItem>> GetLanguagesLookUp();
+        Task<List<SelectionItem>> GetLanguagesLookUpWithCode();
+    }
+}
---
+++ DataAccess/Abstract/IUserClaimRepository.cs
@@ -0,0 +1,16 @@
+﻿using Core.DataAccess;
+using Core.Entities.Concrete;
+using Core.Entities.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Abstract
+{
+    public interface IUserClaimRepository:IEntityRepository<UserClaim>
+    {
+        Task<IEnumerable<SelectionItem>> GetUserClaimSelectedList(int userId);
+        Task<IEnumerable<UserClaim>> BulkInsert(int userId, IEnumerable<UserClaim> userClaims);
+    }
+}
