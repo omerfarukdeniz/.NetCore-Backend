55fcfbf7f3f6ec18254dae43a7c0174914ea96d1
omerfarukdeniz <omerfarukdeniz75@gmail.com>
2021-07-27 19:52:13 +0300

    DArchInMemory added

---
+++ Business/Fakes/DArch/DArchInMemory.cs
@@ -0,0 +1,23 @@
+﻿using DataAccess.Concrete.EntityFramework.Contexts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Business.Fakes.DArch
+{
+    public sealed class DArchInMemory:ProjectDbContext
+    {
+        public DArchInMemory(DbContextOptions<DArchInMemory> options, IConfiguration configuration) : base(options, configuration)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                base.OnConfiguring(optionsBuilder.UseInMemoryDatabase(configuration.GetConnectionString("DArchInMemory")));
+            }
+        }
+    }
+}
---
+++ Business/Fakes/IFakeStore.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Fakes
+{
+    public interface IFakeStore
+    {
+        List<TEntity> Set<TEntity>();
+    }
+}
