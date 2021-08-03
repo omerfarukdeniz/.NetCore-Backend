89967f088d85d3e56a1dba01b8398b987594a2f9
omerfarukdeniz <omerfarukdeniz75@gmail.com>
2021-08-03 17:36:26 +0300

    AuthController Added

---
+++ Business/Handlers/Users/Commands/UserChangePasswordCommand.cs
@@ -0,0 +1,49 @@
+﻿using Business.BusinessAspects;
+using Business.Constants;
+using Core.Aspects.Autofac.Logging;
+using Core.CrossCuttingConcerns.Logging.SeriLog.Loggers;
+using Core.Utilities.Results;
+using Core.Utilities.Security.Hashing;
+using DataAccess.Abstract;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Business.Handlers.Users.Commands
+{
+    [SecuredOperation]
+    public class UserChangePasswordCommand:IRequest<IResult>
+    {
+        public int UserId { get; set; }
+        public string Password { get; set; }
+        public class UserChangePasswordCommandHandler : IRequestHandler<UserChangePasswordCommand, IResult>
+        {
+            private readonly IUserRepository _userRepository;
+            private readonly IMediator _mediator;
+            public UserChangePasswordCommandHandler(IUserRepository userRepository, IMediator mediator)
+            {
+                _userRepository = userRepository;
+                _mediator = mediator;
+            }
+            [LogAspect(typeof(FileLogger))]
+            public async Task<IResult> Handle(UserChangePasswordCommand request, CancellationToken cancellationToken)
+            {
+                var userExits = await _userRepository.GetAsync(u => u.UserId == request.UserId);
+                if (userExits == null)
+                    return new ErrorResult(Messages.UserNotFound);
+
+                HashingHelper.CreatePasswordHash(request.Password, out byte[] passowordHash, out byte[] passwordSalt);
+
+                userExits.PasswordHash = passowordHash;
+                userExits.PasswordSalt = passwordSalt;
+
+                _userRepository.Update(userExits);
+                await _userRepository.SaveChangesAsync();
+                return new SuccessResult(Messages.Updated);
+            }
+        }
+    }
+}
---
+++ WebAPI/Controllers/AuthController.cs
@@ -0,0 +1,84 @@
+﻿using Business.Handlers.Authorizations.Commands;
+using Business.Handlers.Authorizations.Queries;
+using Business.Handlers.Users.Commands;
+using Business.Services.Authentication.Model;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : BaseApiController
+    {
+        private readonly IConfiguration _configuration;
+
+        public AuthController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [ProducesResponseType(typeof(LoginUserResult),200)]
+        [AllowAnonymous]
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] LoginUserQuery loginModel)
+        {
+            var result = await Mediator.Send(loginModel);
+            if (result.Success)
+                return Ok(result);
+            return Unauthorized(result.Message);
+        }
+
+        [AllowAnonymous]
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] RegisterUserCommand registerUser)
+        {
+            var result = await Mediator.Send(registerUser);
+            if (result.Success)
+                return Ok(result);
+
+            return BadRequest(result);
+        }
+
+
+        [HttpPut("forgotpassword")]
+        [AllowAnonymous]
+        public async Task<IActionResult> ForgotPassword([FromBody] ForgotPasswordCommand forgotPassword)
+        {
+            var result = await Mediator.Send(forgotPassword);
+            if (result.Success)
+                return Ok(result);
+
+            return BadRequest(result);
+        }
+
+
+        [HttpPut("changeuserpassword")]
+        public async Task<IActionResult> ChangeUserPassword([FromBody] UserChangePasswordCommand command)
+        {
+            var result = await Mediator.Send(command);
+            if (result.Success)
+                return Ok(result);
+
+            return BadRequest(result);
+        }
+
+        [AllowAnonymous]
+        [HttpPost("verify")]
+        public async Task<IActionResult> Verification([FromBody] VerifyCidQuery verifyCid)
+        {
+            var result = await Mediator.Send(verifyCid);
+            if (result.Success)
+                return Ok(result.Message);
+
+            return BadRequest(result.Message);
+        }
+
+    }
+}
