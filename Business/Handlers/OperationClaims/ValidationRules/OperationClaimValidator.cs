ccfac353ee1872756b06a70b837781a5b7e2a4ae
omerfarukdeniz <omerfarukdeniz75@gmail.com>
2021-08-03 14:03:26 +0300

    UserClaims handler added

---
+++ Business/Handlers/OperationClaims/ValidationRules/OperationClaimValidator.cs
@@ -0,0 +1,23 @@
+﻿using Business.Handlers.OperationClaims.Commands;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Handlers.OperationClaims.ValidationRules
+{
+    public class CreateOperationClaimValidator:AbstractValidator<CreateOperationClaimCommand>
+    {
+        public CreateOperationClaimValidator()
+        {
+            RuleFor(x => x.ClaimName).NotEmpty();
+        }
+    }
+    public class UpdateOperationClaimValidator : AbstractValidator<UpdateOperationClaimCommand>
+    {
+        public UpdateOperationClaimValidator()
+        {
+            RuleFor(x => x.Id).NotEmpty();
+        }
+    }
+}
---
+++ Business/Handlers/UserClaims/Commands/CreateUserClaimsInternalCommand.cs
@@ -0,0 +1,49 @@
+﻿using Business.Constants;
+using Core.Entities.Concrete;
+using Core.Utilities.Results;
+using DataAccess.Abstract;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Business.Handlers.UserClaims.Commands
+{
+    public class CreateUserClaimsInternalCommand: IRequest<IResult>
+    {
+        public int UserId { get; set; }
+        public IEnumerable<OperationClaim> OperationClaims { get; set; }
+
+        public class CreateUserClaimsInternalCommandHandler : IRequestHandler<CreateUserClaimsInternalCommand, IResult>
+        {
+            private readonly IUserClaimRepository _userClaimRepository;
+            public CreateUserClaimsInternalCommandHandler(IUserClaimRepository userClaimRepository)
+            {
+                _userClaimRepository = userClaimRepository;
+            }
+            public async Task<IResult> Handle(CreateUserClaimsInternalCommand request, CancellationToken cancellationToken)
+            {
+                foreach (var claim in request.OperationClaims)
+                {
+                    if (await DoesClaimExistsForUser(new UserClaim { ClaimId = claim.Id, UserId = request.UserId }))
+                        continue;
+
+                    _userClaimRepository.Add(new UserClaim
+                    {
+                        ClaimId = claim.Id,
+                        UserId = request.UserId
+                    });
+                }
+                await _userClaimRepository.SaveChangesAsync();
+                return new SuccessResult(Messages.Added);
+            }
+
+            private async Task<bool> DoesClaimExistsForUser(UserClaim userClaim)
+            {
+                return (await _userClaimRepository.GetAsync(x => x.UserId == userClaim.UserId && x.ClaimId == userClaim.ClaimId)) is null ? false : true;
+            }
+        }
+    }
+}
